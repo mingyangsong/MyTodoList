36c8fb4f9572025fb6ad24916b2412169ae7781a
mingysong <mingy.song@gmail.com>
2014-03-29 20:25:28 -0400

    Initial Commit

---
+++ MyTodoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  MyTodoList
+//
+//  Created by Mingyang Song on 3/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyTodoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyTodoList
+//
+//  Created by Mingyang Song on 3/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
